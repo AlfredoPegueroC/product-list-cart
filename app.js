773427d7650cf19720488fbe31a5bdb301679d67
unknown <alfredocapellan20@gmail.com>
2024-08-08 22:20:33 -0400

    createcard added

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
-const container = document.querySelector(".card__container")
+const container = document.querySelector(".card__container");
+const btn = document.getElementsByClassName("btn");
 
-async function getMenu(){
-  const res = await fetch("./data.json")
-  const data = await res.json()
+document.addEventListener("DOMContentLoaded", (e) => getMenu());
 
-  data.forEach(element => {
-    container.innerHTML += `
-      <div class="card">
-          <img src="${element.image.mobile}" alt="plate-image"/>
-          <button class="btn">
-            <span
-              ><img
-                src="./assets/images/icon-add-to-cart.svg"
-                alt="cart" /></span
-            >Add to Cart
-          </button>
-          <div class="card__content">
-            <h4 class="category">${element.category}</h4>
-            <h3 class="name">${element.name}</h3>
-            <span class="price">$${element.price.toFixed(2)}</span>
-          </div>
-        </div>
-    `
-  });
+
+function createCard(container, element) {
+  // DIV .card
+  const div = document.createElement("div");
+  div.setAttribute("class", "card");
+  // IMAGES TAG
+  let img = document.createElement("img");
+  img.src = `${element.image.mobile}`;
+  img.alt = "plate-image";
+
+  // BUTTON TAG
+  let button = document.createElement("button");
+  button.setAttribute("class", "btn");
+  let btn_img = document.createElement("img");
+  btn_img.src = "./assets/images/icon-add-to-cart.svg";
+  btn_img.alt = "cart";
+  button.textContent = "Add to cart";
+  button.appendChild(btn_img);
+  // DIV CLASS CARD__CONTENT
+  let content = document.createElement("div");
+  content.setAttribute("class", "card__content");
+
+  let h4 = document.createElement("h4");
+  h4.setAttribute("class", "category");
+  h4.textContent = `${element.category}`;
+
+  let h3 = document.createElement("h3");
+  h3.setAttribute("class", "name");
+  h3.textContent = `${element.name}`;
+
+  let span = document.createElement("span");
+  span.setAttribute("class", "price");
+  span.textContent = `$${element.price.toFixed(2)}`;
+
+  content.appendChild(h4);
+  content.appendChild(h3);
+  content.appendChild(span);
+
+  // APPEND CHILLD TO DIV
+  div.appendChild(img);
+  div.appendChild(button);
+  div.appendChild(content);
+
+  // ADD TO CONTAINER OF THE ELEMENT
+  container.appendChild(div);
 }
-getMenu()
+
+async function getMenu() {
+  const res = await fetch("./data.json");
+  const data = await res.json();
+
+  data.forEach((element) => {
+    createCard(container, element);
+  });
+
+  Array.from(btn).forEach(items => {
+    console.log(items);
+  })
+}
+
+Array.from(btn).forEach(items => {
+  console.log(items);
+})
+
